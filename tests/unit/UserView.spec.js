b35bb4179c93ad6834628430ae41a25739c167af
jay <dj.min43@gmail.com>
2021-07-11 18:10:32 +0900

    testing vuex in progress

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,18 +1,41 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import UserView from '@/views/UserView'
 import VUserSearchForm from '@/components/VUserSearchForm'
 import VUserProfile from '@/components/VUserProfile'
+import initialState from '@/store/state'
+import userFixture from './fixtures/user'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('UserView', () => {
+    let state
+
     const build = () => {
-        const wrapper = shallowMount(UserView)
+        const wrapper = shallowMount(UserView, {
+           localVue,
+           store: new Vuex.Store({ state })
+        })
         return {
             wrapper,
             userSearchForm: () => wrapper.findComponent(VUserSearchForm),
             userProfile: () => wrapper.findComponent(VUserProfile)
         }
-    
     }
+
+    beforeEach(() => {
+        state = { ...initialState }
+    })
+
+    it('passes a binded user prop to user profile component', () => {
+        // arrange
+        state.user = userFixture
+        const { userProfile } = build()
+
+        // assert
+        expect(userProfile().vm.user).toBe(state.user)
+    })
 
     it('renders the component', () => {
         // arrange
@@ -29,6 +52,19 @@ describe('UserView', () => {
         // assert
         expect(userSearchForm().exists()).toBe(true)
         expect(userProfile().exists()).toBe(true) 
+    })
+
+    it('passes a binded user prop to user profile component', () => {
+        // arrange
+        const { wrapper, userProfile } = build()
+        wrapper.setData({
+            user: {
+                name: 'Jay'
+            }
+        })
+
+        // assert
+        expect(userProfile().vm.user).toBe(wrapper.vm.user)
     })
 
 })
