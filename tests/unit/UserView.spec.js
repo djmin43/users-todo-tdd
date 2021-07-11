50589e12a5e76442618eb83985a67271a47ee43f
jay <dj.min43@gmail.com>
2021-07-11 13:37:50 +0900

    unit test on component works.

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import UserView from '@/views/UserView'
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
 
 describe('UserView', () => {
     it('renders the component', () => {
@@ -8,5 +10,16 @@ describe('UserView', () => {
 
         // assert
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('renders main child components', () => {
+        // arrange
+        const wrapper = shallowMount(UserView)
+        const userSearchForm = wrapper.findComponent(VUserSearchForm)
+        const userProfile = wrapper.findComponent(VUserProfile)
+
+        // assert
+        expect(userSearchForm.exists()).toBe(true)
+        expect(userProfile.exists()).toBe(true)
     })
 })
